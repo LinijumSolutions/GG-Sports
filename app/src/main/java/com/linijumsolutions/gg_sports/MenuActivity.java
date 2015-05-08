55b6f9748aa0753225a15a1a330d03eb12383c20
MantasN <mantasneviera@gmail.com>
2015-05-08 13:39:23 +0300

    empty project

---
+++ app/src/main/java/com/linijumsolutions/gg_sports/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.linijumsolutions.gg_sports;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MenuActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+}
